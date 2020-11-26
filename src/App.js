a6326c5a4ec47a946edb6476136f62f91b8d32aa
chanyang719 <chanyang0719@gmail.com>
2020-11-26 14:03:49 -0800

    Add search box and website update by search_data

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ class App extends Component{
   constructor(){
     super();
     this.state = {
-      movies: []
+      movies: [],
+      searchField: 'Avengers'
     };
+    
   }
 
   componentDidMount(){
@@ -18,10 +20,29 @@ class App extends Component{
     // .then(result => console.log(result))
   }
 
+  feachMovieData(){
+    console.log(this.state.searchField)
+    fetch(`http://api.themoviedb.org/3/search/movie?query=${this.state.searchField}&api_key=c0cfde6eeaf28982f2de2403db46d141`)
+    .then(response => response.json())
+    .then(data => this.setState({movies:data.results}));
+  }
+
   render() {
+    // this.feachMovieData(this.state.searchName)
     console.log(this.state.movies)
     return (
-      <CardList movies = {this.state.movies}/>
+      <div className = 'App'>
+        <input 
+          type = 'search'
+          placeholder = 'search movies'
+          onChange = {e => this.setState({searchField: e.target.value})}
+        />
+        <button
+          onClick = {() => this.feachMovieData()} 
+        />
+        <CardList movies = {this.state.movies}/>
+      </div>
+      
     );
   }
 }
